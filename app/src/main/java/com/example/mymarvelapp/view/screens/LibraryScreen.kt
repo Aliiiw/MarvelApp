3fdb7a72f0eee988209e7b374523551918525cbb
Ali Rahimi <1aliirahimi1@gmail.com>
2023-03-18 17:36:17 +0330

    add dummy screens for now

---
+++ app/src/main/java/com/example/mymarvelapp/view/screens/LibraryScreen.kt
@@ -0,0 +1,9 @@
+package com.example.mymarvelapp.view.screens
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun LibraryScreen() {
+    Text(text = "Library Screen")
+}
